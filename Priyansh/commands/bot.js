30e074f64f6df9752a18fd04d778029d560b1975
Rangix1 <168188397+Rangix1@users.noreply.github.com>
2025-04-19 04:14:46 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -1,116 +1,118 @@
 const fs = global.nodemodule["fs-extra"];
+
 module.exports.config = {
-  name: "goibot",
-  version: "1.0.2",
-  hasPermssion: 0,
-  credits: "Fixed By Rudra + Typing by ChatGPT",
-  description: "goibot",
-  commandCategory: "Noprefix",
-  usages: "noprefix",
-  cooldowns: 5,
+    name: "goibot",
+    version: "1.0.2",
+    hasPermssion: 0,
+    credits: "Fixed By Rudra + Typing by ChatGPT",
+    description: "goibot",
+    commandCategory: "Noprefix",
+    usages: "noprefix",
+    cooldowns: 5,
 };
 
-module.exports.handleEvent = async function ({ api, event, args, Threads, Users }) {
-  const moment = require("moment-timezone");
-  const time = moment.tz("Asia/Kolkata").format("DD/MM/YYYY || HH:mm:ss");
+module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
+    const moment = require("moment-timezone");
+    const time = moment.tz("Asia/Kolkata").format("DD/MM/YYYY || HH:mm:ss");
 
-  const { threadID, messageID, body } = event;
-  const name = await Users.getNameUser(event.senderID);
+    const { threadID, messageID, senderID, body } = event;
 
-  const tl = [
-    "Kya Tu ELvish Bhai Ke Aage Bolega🙄",
-    "Cameraman Jaldi Focus Kro 📸",
-    "Lagdi Lahore di aa🙈",
-    "Chay pe Chaloge",
-    "Mere liye Chay Bana Kar LA ,Pura din Dekho Bot BoT🙄",
-    "Din vicho tere Layi Teym Kadd ke, Kardi me Promise Milan aungi",
-    "Yee bat Delhi tak jayegi",
-    "Je koi shaq ni , Kari check ni",
-    "ME HERAAN HU KI TUM BINA DIMAG KESE REH LETE HO☹️",
-    "sheHar me Hai rukka baeje Rao Saab ka🙄",
-    "Bewafa Nikali re tu🙂🤨",
-    "Systemmmmmmm😴",
-    "Leja Leja tenu 7 samundra paar🙈🙈",
-    "Laado puche manne kyu tera rang kala",
-    "Moye moye moye moye🙆🏻‍♀🙆🏻‍♀",
-    "Ye dukh kahe nahi khatm hota 🙁",
-    "Tum to dokebaz ho",
-    "you just looking like a wow😶",
-    "Mera aasmaan dhunde teri zamin",
-    "Kal ana abhi lunch time hai",
-    "Jab dekho B0T B0T b0T😒😒",
-    "Chhodo na koi dekh lega🤭",
-    "Kab ayega mere banjaare",
-    "Tum wahi ho na ,jisko.me.nahi janti 🙂",
-    "Ye I love you kya hota hai",
-    "Sunai deta hai mujhe behri nahi hu me 😒",
-    "so elegant, so beautiful , just looking like a wow🤭",
-    "began🙂",
-    "Aayein🤔",
-    "I Love you baby , mera recharge khtm hone wala h",
-    "paani paani uncle ji",
-    "apne Labhar ko dhoka do , daling hme bhi moka do🙈",
-    "Arry Bas Kar🤣😛",
-    "Me ni To Kon Be",
-    "naam adiya kumar 7vi kaksha me padhte hai favret subject begon😘",
-    "Mera Dimag Mat Khaya kro😒😒",
-    "Chuppp Saatvi Fail😒",
-    "Saste Nashe Kab Band kroge",
-    "Mai Jaanu Ke sath Busy hu yar, mujhe mat balao",
-    "Haye Jaanu Mujhe Yaad Kiya🙈",
-    "Hayee ese mt bulaya kro, mujhe sharm aati h",
-    "System pe system betha rahi chhori bot ki",
-    "Naach meri Bulbul tujhe pesa milega",
-    "me idhar se hu aap kidhar se ho",
-    "Khelega Free Fire🙈🙈",
-    "aye haye oye hoye aye haye oye hoye😍 bado badi bado badi😘",
-    "e halo bhai darr rha hai kya",
-    "akh ladi bado badi",
-    "haaye garmi😕",
-    "Ao kabhi haweli pe😍",
-    "Khelega Free Fire🥴",
-    "Hallo bai tu darr raha hai kya",
-    "janu bula raha h mujhe",
-    "I cant live without you babu😘",
-    "haa meri jaan",
-    "Agye Phirse Bot Bot Krne🙄",
-    "konse color ki jacket pehne ho umm btao na😚",
-    "dhann khachh booyaah"
-  ];
+    const name = await Users.getNameUser(senderID);
 
-  // IB reply system
-  if (event.body?.toLowerCase().startsWith("bot")) {
-    const rand = tl[Math.floor(Math.random() * tl.length)];
+    const tl = [
+        "Kya Tu ELvish Bhai Ke Aage Bolega🙄",
+        "Cameraman Jaldi Focus Kro 📸",
+        "Lagdi Lahore di aa🙈",
+        "Chay pe Chaloge",
+        "Mere liye Chay Bana Kar LA ,Pura din Dekho Bot BoT🙄",
+        "Din vicho tere Layi Teym Kadd ke, Kardi me Promise Milan aungi",
+        "Yee bat Delhi tak jayegi",
+        "Je koi shaq ni , Kari check ni",
+        "ME HERAAN HU KI TUM BINA DIMAG KESE REH LETE HO☹️",
+        "sheHar me Hai rukka baeje Rao Saab ka🙄",
+        "Bewafa Nikali re tu🙂🤨",
+        "Systemmmmmmm😴",
+        "Leja Leja tenu 7 samundra paar🙈🙈",
+        "Laado puche manne kyu tera rang kala",
+        "Moye moye moye moye🙆🏻‍♀🙆🏻‍♀",
+        "Ye dukh kahe nahi khatm hota 🙁",
+        "Tum to dokebaz ho",
+        "you just looking like a wow😶",
+        "Mera aasmaan dhunde teri zamin",
+        "Kal ana abhi lunch time hai",
+        "Jab dekho B0T B0T b0T😒😒",
+        "Chhodo na koi dekh lega🤭",
+        "Kab ayega mere banjaare",
+        "Tum wahi ho na ,jisko.me.nahi janti 🙂",
+        "Ye I love you kya hota hai",
+        "Sunai deta hai mujhe behri nahi hu me 😒",
+        "so elegant, so beautiful , just looking like a wow🤭",
+        "began🙂",
+        "Aayein🤔",
+        "I Love you baby , mera recharge khtm hone wala h",
+        "paani paani uncle ji",
+        "apne Labhar ko dhoka do , daling hme bhi moka do🙈",
+        "Arry Bas Kar🤣😛",
+        "Me ni To Kon Be",
+        "naam adiya kumar 7vi kaksha me padhte hai favret subject begon😘",
+        "Mera Dimag Mat Khaya kro😒😒",
+        "Chuppp Saatvi Fail😒",
+        "Saste Nashe Kab Band kroge",
+        "Mai Jaanu Ke sath Busy hu yar, mujhe mat balao",
+        "Haye Jaanu Mujhe Yaad Kiya🙈",
+        "Hayee ese mt bulaya kro, mujhe sharm aati h",
+        "System pe system betha rahi chhori bot ki",
+        "Naach meri Bulbul tujhe pesa milega",
+        "me idhar se hu aap kidhar se ho",
+        "Khelega Free Fire🙈🙈",
+        "aye haye oye hoye aye haye oye hoye😍 bado badi bado badi😘",
+        "e halo bhai darr rha hai kya",
+        "akh ladi bado badi",
+        "haaye garmi😕",
+        "Ao kabhi haweli pe😍",
+        "Khelega Free Fire🥴",
+        "Hallo bai tu darr raha hai kya",
+        "janu bula raha h mujhe",
+        "I cant live without you babu😘",
+        "haa meri jaan",
+        "Agye Phirse Bot Bot Krne🙄",
+        "konse color ki jacket pehne ho umm btao na😚",
+        "dhann khachh booyaah"
+    ];
 
-    // ✅ Show Typing Indicator
-    api.sendTypingIndicator(threadID, true);
+    // Reply for group chat
+    if (event.body?.toLowerCase().startsWith("bot")) {
+        const rand = tl[Math.floor(Math.random() * tl.length)];
 
-    const msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : Rudra🌹`
-    };
+        // Show typing indicator in the group
+        api.sendTypingIndicator(threadID, true);
 
-    // ✅ Hide Typing Indicator
-    api.sendTypingIndicator(threadID, false);
+        const msg = {
+            body: `🔶${name}🔶, \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : Rudra🌹`
+        };
 
-    return api.sendMessage(msg, threadID, messageID);
-  }
+        // Hide typing indicator in the group
+        api.sendTypingIndicator(threadID, false);
 
-  // Inbox reply system (checking if message comes in inbox)
-  if (event.body && event.isInbox) {
-    const rand = tl[Math.floor(Math.random() * tl.length)];
+        return api.sendMessage(msg, threadID, messageID);
+    }
 
-    // ✅ Show Typing Indicator
-    api.sendTypingIndicator(threadID, true);
+    // Reply for inbox messages
+    if (event.threadID === senderID) {
+        const rand = tl[Math.floor(Math.random() * tl.length)];
 
-    const msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : Rudra🌹`
-    };
+        // Show typing indicator in inbox
+        api.sendTypingIndicator(threadID, true);
 
-    // ✅ Hide Typing Indicator
-    api.sendTypingIndicator(threadID, false);
+        const msg = {
+            body: `🔶${name}🔶, \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : Rudra🌹`
+        };
 
-    return api.sendMessage(msg, threadID, messageID);
-  }
+        // Hide typing indicator in inbox
+        api.sendTypingIndicator(threadID, false);
+
+        return api.sendMessage(msg, threadID, messageID);
+    }
 };
 
-module.exports.run = function ({ api, event, client, __GLOBAL }) {};
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
